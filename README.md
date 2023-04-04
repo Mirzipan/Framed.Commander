b92dc823475016a6bc46668f4ba287c8c21a6a37
Mirzipan <mfarkas@pixelfederation.com>
2023-04-04 20:32:41 +0200

    🎉first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+[![openupm](https://img.shields.io/npm/v/net.mirzipan.framed.commander?label=openupm&registry_uri=https://package.openupm.com)](https://openupm.com/packages/net.mirzipan.framed.definitions/) ![GitHub](https://img.shields.io/github/license/Mirzipan/Framed.Commander)
+
 # Framed.Commander
-Simple command system with requests, validation, and execution
+
+Simple module for commands for Framed.
+
+
---
+++ Runtime/Configurations/CommandModuleConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Mirzipan.Framed.Configurations;
+using UnityEngine;
+
+namespace Mirzipan.Commander.Runtime.Configurations
+{
+    [CreateAssetMenu(fileName = "CommanderConfiguration", menuName = "Framed/Commander Configuration", order = 1200)]
+    public class CommandModuleConfiguration: ConfigurationScriptableObject
+    {        
+        public override void AddBindings()
+        {
+        }
+    }
+}
